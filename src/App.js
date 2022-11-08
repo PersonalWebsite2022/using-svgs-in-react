e4ab5ae1832ba77a6c69db1060b76daceb4da166
PersonalWebsite2022 <PersonalWebsite2022@gmail.com>
2022-11-08 10:31:22 -0800

    Using SVGs in React

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import './App.css';
+import { AlertExclamation } from './components/AlertExclamation'
+
+function App() {
+  return (
+    <div className="App">
+        <AlertExclamation />
+    </div>
+  );
+}
+
+/* function App() {
+  return (
+    <div className="App">
+        <img src={logo} style={{ height: "100px" }} alt="logo" />
+    </div>
+  );
+} */
+
+export default App;
